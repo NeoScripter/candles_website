c8b69c145bf469d8de40af6f9923ee46ac8fb5f5
unknown <sange0337@gmail.com>
2024-05-20 23:40:26 +0800

    Added the popups

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
+    // Function to toggle visibility of an overlay
+    function toggleOverlay(button, overlayClass, isVisible) {
+        let container = button.closest('body'); 
 
+        const overlay = container.querySelector(overlayClass);
+        if (overlay) {
+            overlay.classList.toggle('visible-pop-up', isVisible);
+        }
+    }
+
+    function setupButtonListeners(buttonSelector, overlaySelector, shouldShow) {
+        document.querySelectorAll(buttonSelector).forEach(button => {
+            button.addEventListener('click', () => toggleOverlay(button, overlaySelector, shouldShow));
+        });
+    }
+
+    setupButtonListeners('.callback', '.webform-popup-overlay', true);
+    setupButtonListeners('.popup-close-btn', '.webform-popup-overlay', false);
+    setupButtonListeners('.wk-order', '.webform-workshop-overlay', true);
+    setupButtonListeners('.popup-close-btn', '.webform-workshop-overlay', false);
 });
